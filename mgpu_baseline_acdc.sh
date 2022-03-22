d7b72f43020e71db2b455ad59b79c8ab9e48ddcf
Qi YAN <qiyanthor@gmail.com>
2022-03-22 02:16:17 -0700

    add mgpu training scripts

--- mgpu_baseline_acdc.sh
+++ mgpu_baseline_acdc.sh
@@ -1,8 +1,11 @@
 export EXP_NAME="ACDC_fog"
-export CONFIG_FILE="configs/swin/upernet_swin_small_patch4_window7_512x512_20k_ACDC.py"
+#export CONFIG_FILE="configs/swin/upernet_swin_small_patch4_window7_512x512_20k_ACDC.py"
+export CONFIG_FILE="configs/swin/upernet_swin_small_patch4_window7_512x512_80k_ACDC.py"
 export PRETRAINED_MODEL_PATH="weights/swin_small_patch4_window7_224.pth"
 export WORK_DIR="runs/${EXP_NAME}"
+export NUM_GPUS=2
 
 mkdir -p $WORK_DIR
 
-python tools/train.py ${CONFIG_FILE} --work-dir ${WORK_DIR} --options model.pretrained=${PRETRAINED_MODEL_PATH} model.backbone.use_checkpoint=True
+python tools/train.py ${CONFIG_FILE} --gpus $NUM_GPUS --work-dir ${WORK_DIR} --options model.pretrained=${PRETRAINED_MODEL_PATH} model.backbone.use_checkpoint=True
+
