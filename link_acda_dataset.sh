997dd0f07b71bb5d5f0226e9b19077cfb8955184
Qi YAN <qiyanthor@gmail.com>
2022-03-22 02:00:52 -0700

    update setup config

--- link_acda_dataset.sh
+++ link_acda_dataset.sh
@@ -12,11 +12,11 @@ do
   ln -s $ACDA_PATH/gt/$keyword/* ACDC/$keyword/gtFine
 done
 
+cd ../..
 pip install -r requirements.txt
 pip install torch==1.8.0+cu111 torchvision==0.9.0+cu111 torchaudio==0.8.0 -f https://download.pytorch.org/whl/torch_stable.html
 pip install "mmcv-full>=1.1.4,<=1.3.0" -f https://download.openmmlab.com/mmcv/dist/cu111/torch1.8.0/index.html
 
-cd ../..
 pip install -e .
 
 mkdir -p weights
