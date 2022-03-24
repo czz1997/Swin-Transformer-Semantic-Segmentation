029bedf4949e965870c4c345a8e25f480d28e808
Qi YAN <qiyanthor@gmail.com>
2022-03-24 00:55:44 -0700

    add leave-one-out nouveau dataset split

--- link_acda_dataset.sh
+++ link_acda_dataset.sh
@@ -3,13 +3,45 @@ export ACDA_PATH=/home/qiyan/datarepo/datasets/ACDC
 mkdir -p data/datasets
 cd data/datasets
 
+# create link for the vanilla ACDC data structure
 for keyword in "fog" "night" "rain" "snow"
 do
-  mkdir -p ACDC/$keyword/leftImg8bit
-  ln -s $ACDA_PATH/rgb_anon/$keyword/* ACDC/$keyword/leftImg8bit
+  mkdir -p ACDC_vanilla/$keyword/leftImg8bit
+  ln -sf $ACDA_PATH/rgb_anon/$keyword/* ACDC_vanilla/$keyword/leftImg8bit
 
-  mkdir -p ACDC/$keyword/gtFine
-  ln -s $ACDA_PATH/gt/$keyword/* ACDC/$keyword/gtFine
+  mkdir -p ACDC_vanilla/$keyword/gtFine
+  ln -sf $ACDA_PATH/gt/$keyword/* ACDC_vanilla/$keyword/gtFine
+done
+
+export ACDC_VANILLA_PATH=$(pwd)/ACDC_vanilla
+# re-organize the four scenes into leave-one-out setting
+# i.e., the training for "fog" is the train+val of data from "night", "rain" and "snow" scenes
+# the validation for "fog" is its own train+val data
+# all testing data is omitted
+
+for keyword in "fog" "night" "rain" "snow"
+do
+  mkdir -p ACDC_nouveau/$keyword/leftImg8bit/train
+  mkdir -p ACDC_nouveau/$keyword/leftImg8bit/val
+  mkdir -p ACDC_nouveau/$keyword/gtFine/train
+  mkdir -p ACDC_nouveau/$keyword/gtFine/val
+  for key_inner in "fog" "night" "rain" "snow"
+  do
+    if [ "$key_inner" = "$keyword" ]; then
+        # scene A's train+val now becomes scene A's val
+        ln -sf $ACDC_VANILLA_PATH/$key_inner/leftImg8bit/train/* ACDC_nouveau/$keyword/leftImg8bit/val
+        ln -sf $ACDC_VANILLA_PATH/$key_inner/leftImg8bit/val/* ACDC_nouveau/$keyword/leftImg8bit/val
+        ln -sf $ACDC_VANILLA_PATH/$key_inner/gtFine/train/* ACDC_nouveau/$keyword/gtFine/val
+        ln -sf $ACDC_VANILLA_PATH/$key_inner/gtFine/val/* ACDC_nouveau/$keyword/gtFine/val
+    else
+        # scene non-A's train+val now becomes scene A's train
+        ln -sf $ACDC_VANILLA_PATH/$key_inner/leftImg8bit/train/* ACDC_nouveau/$keyword/leftImg8bit/train
+        ln -sf $ACDC_VANILLA_PATH/$key_inner/leftImg8bit/val/* ACDC_nouveau/$keyword/leftImg8bit/train
+        ln -sf $ACDC_VANILLA_PATH/$key_inner/gtFine/train/* ACDC_nouveau/$keyword/gtFine/train
+        ln -sf $ACDC_VANILLA_PATH/$key_inner/gtFine/val/* ACDC_nouveau/$keyword/gtFine/train
+    fi
+  done
+
 done
 
 cd ../..
