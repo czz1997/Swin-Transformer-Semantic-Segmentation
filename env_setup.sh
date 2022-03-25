c1a1ec10acaa85e0aea94afa99ac3618e735ec31
Qi YAN <qiyanthor@gmail.com>
2022-03-24 22:08:13 -0700

    clean up scripts to setup environment

---
+++ env_setup.sh
@@ -0,0 +1,10 @@
+pip install -r requirements.txt
+pip install torch==1.8.0+cu111 torchvision==0.9.0+cu111 torchaudio==0.8.0 -f https://download.pytorch.org/whl/torch_stable.html
+pip install "mmcv-full>=1.1.4,<=1.3.0" -f https://download.openmmlab.com/mmcv/dist/cu111/torch1.8.0/index.html
+
+pip install -e .
+
+mkdir -p weights
+
+wget https://github.com/SwinTransformer/storage/releases/download/v1.0.0/swin_tiny_patch4_window7_224.pth -P weights
+wget https://github.com/SwinTransformer/storage/releases/download/v1.0.0/swin_small_patch4_window7_224.pth -P weights
