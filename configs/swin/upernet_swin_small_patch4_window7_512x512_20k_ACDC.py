ef9f0179200f9dcfc1f59a9682faa578ada00eb8
zchen <chenzze1@student.ubc.ca>
2022-03-30 09:40:13 -0700

    60k schedule

--- configs/swin/upernet_swin_small_patch4_window7_512x512_20k_ACDC.py
+++ configs/swin/upernet_swin_small_patch4_window7_512x512_20k_ACDC.py
@@ -1,6 +1,6 @@
 _base_ = [
     '../_base_/models/upernet_swin.py', '../_base_/datasets/cityscapes.py',
-    '../_base_/default_runtime.py', '../_base_/schedules/schedule_20k.py'
+    '../_base_/default_runtime.py', '../_base_/schedules/schedule_60k.py'
 ]
 model = dict(
     backbone=dict(
@@ -23,7 +23,7 @@ model = dict(
     ))
 
 # AdamW optimizer, no weight decay for position embedding & layer norm in backbone
-optimizer = dict(_delete_=True, type='AdamW', lr=0.00001, betas=(0.9, 0.999), weight_decay=0.01,
+optimizer = dict(_delete_=True, type='AdamW', lr=0.00002, betas=(0.9, 0.999), weight_decay=0.01,
                  paramwise_cfg=dict(custom_keys={'absolute_pos_embed': dict(decay_mult=0.),
                                                  'relative_position_bias_table': dict(decay_mult=0.),
                                                  'norm': dict(decay_mult=0.)}))
