193d24f685b2506f9f188aae1a94dc1136b4c6b7
Qi YAN <qiyanthor@gmail.com>
2022-04-17 02:22:23 -0700

    add swinTF base pretrained weight

--- env_setup.sh
+++ env_setup.sh
@@ -8,3 +8,4 @@ mkdir -p weights
 
 wget https://github.com/SwinTransformer/storage/releases/download/v1.0.0/swin_tiny_patch4_window7_224.pth -P weights
 wget https://github.com/SwinTransformer/storage/releases/download/v1.0.0/swin_small_patch4_window7_224.pth -P weights
+wget https://github.com/SwinTransformer/storage/releases/download/v1.0.0/swin_base_patch4_window7_224.pth -P weights
